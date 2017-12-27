414dcecf4f4b720a44e7b1b81285741a533c5c1e
lazrossi <lazrossi@student.42.fr>
2017-12-27 08:53:04 +0100

    transformation du parsing en tableaux d'int

--- main.c
+++ main.c
@@ -9,3 +9,18 @@
 /*   Updated: 2017/12/22 13:40:43 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+int		main(int ac, char **av)
+{
+	(void)ac;
+
+	long ***tab;
+
+	tab = NULL;
+	if (av[1])
+		tab = ft_parsing(av[1])
+	else 
+		ft_putstr("Pass a file to FDF to launch the program\n");
+	return (0);
+}
